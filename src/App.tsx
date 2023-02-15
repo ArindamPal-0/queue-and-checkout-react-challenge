9338f179636c15bd2c02294a864a7c25ba36f26c
Arindam Pal <arindampal2000@gmail.com>
2023-02-15 17:42:46 +0530

    simulation State and Timer logic
    - added simulation controls
    - added play, pause, stop icons svg
    - added simulation state reducer
    - added timer state and ui
    - change timer value with setInterval and state = "running"

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,100 @@
-import { useEffect, useState } from "react";
+import { useEffect, useReducer, useState } from "react";
+import pauseIcon from "./assets/player-pause.svg";
+import playIcon from "./assets/player-play.svg";
+import stopIcon from "./assets/player-stop.svg";
 import Queue from "./components/Queue";
+
+type SimulationState = "running" | "paused" | "stopped";
 
 function App() {
     const [input, setInput] = useState<number>(0);
     const [queues, setQueues] = useState<number[][]>([]);
 
+    function stateReducer(
+        state: SimulationState,
+        action: { next: SimulationState }
+    ) {
+        switch (action.next) {
+            case "running":
+                if (state === "paused" || state === "stopped") {
+                    return action.next;
+                } else {
+                    // throw new Error("running -> running ❌");
+                }
+                break;
+            case "paused":
+                if (state === "running") {
+                    return action.next;
+                } else {
+                    // throw new Error("stopped | paused -> paused ❌");
+                }
+                break;
+            case "stopped":
+                if (state !== "stopped") {
+                    return action.next;
+                } else {
+                    // throw new Error("Simulation is already stopped");
+                }
+                break;
+            // default:
+            // throw new Error(`Unknown Action ${action.next}`);
+        }
+        return state;
+    }
+
+    const initialState: SimulationState = "stopped";
+
+    const [state, stateDispatch] = useReducer(stateReducer, initialState);
+
+    const [timer, setTimer] = useState<number>(0);
+
+    // on State Change
     useEffect(() => {
-        initializeQueues();
+        console.log("onStateChange");
+        if (state === "stopped") {
+            setTimer(0);
+        }
+
+        // TODO: make efficient setInterval
+        // i.e. only create setInterval when state = "running"
+
+        // setup setInterval on Component mount
+        const id = setInterval(() => {
+            if (state === "running") {
+                setTimer((value) => value + 1);
+            }
+            // console.log(`${id}: running`);
+        }, 1000);
+        console.log(`setInterval setup with id: ${id}.`);
 
         return () => {
+            // clear setInterval on Component unmount
+            clearInterval(id);
+            console.log(`setInterval with id: ${id} cleared.`);
+        };
+    }, [state]);
+
+    // on Timer Change
+    useEffect(() => {
+        console.log(`timer: ${timer}`);
+        // do simulation stuffs
+    }, [timer]);
+
+    // on Component Mount
+    useEffect(() => {
+        // initialize state to "stopped"
+        stateDispatch({ next: "stopped" });
+
+        // set queues to initial value
+        initializeQueues();
+
+        // set timer value to zero
+        setTimer(0);
+
+        return () => {
+            stateDispatch({ next: "stopped" });
             setQueues([]);
+            setTimer(0);
         };
     }, []);
 
@@ -82,11 +167,41 @@ function App() {
                 </form>
                 <section className="flex w-full items-center justify-center gap-3 border-b border-t border-b-blue-200 border-t-blue-200 p-5">
                     <button
-                        className="rounded bg-red-600 px-3 py-1 text-xl text-white hover:bg-red-500"
+                        className="rounded bg-red-600 px-3 py-2 text-xl text-white hover:bg-red-500"
                         onClick={() => initializeQueues()}
                     >
                         Reset
                     </button>
+                    <div className="flex items-center justify-center gap-2 rounded border-2 border-red-600 px-3 py-1">
+                        <span className="text-xl text-red-600">Simulate</span>
+                        <button
+                            className="rounded-full bg-red-600 p-1 disabled:bg-red-400"
+                            disabled={state === "running"}
+                            onClick={() => stateDispatch({ next: "running" })}
+                        >
+                            <img src={playIcon} alt="play" />
+                        </button>
+                        <button
+                            className="rounded-full bg-red-600 p-1 disabled:bg-red-400"
+                            disabled={state !== "running"}
+                            onClick={() => stateDispatch({ next: "paused" })}
+                        >
+                            <img src={pauseIcon} alt="pause" />
+                        </button>
+                        <button
+                            className="rounded-full bg-red-600 p-1 disabled:bg-red-400"
+                            disabled={state === "stopped"}
+                            onClick={() => stateDispatch({ next: "stopped" })}
+                        >
+                            <img src={stopIcon} alt="stop" />
+                        </button>
+                    </div>
+                    <div className="flex items-center justify-center gap-2 rounded border-2 border-red-600 px-3 py-1">
+                        <span className="text-xl text-red-600">Timer: </span>
+                        <span className="text-2xl font-semibold text-red-600">
+                            {timer}
+                        </span>
+                    </div>
                 </section>
                 <section className="flex flex-col gap-1 py-5">
                     <h2 className="text-xl font-semibold text-blue-600 underline">
